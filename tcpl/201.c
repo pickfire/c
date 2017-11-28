d2518f0fc1e3a43538d1253401c8629882ecfd30
Ivan Tham <pickfire@riseup.net>
2017-11-29 00:35:13 +0800

    [tcpl] Add 201.c direct computation

--- tcpl/201.c
+++ tcpl/201.c
@@ -18,10 +18,14 @@ int main()
 	printf("signed long\t%ld:%ld\n", LONG_MIN, LONG_MAX);
 	printf("unsigned long\t0:%lu\n", ULONG_MAX);
 
-	char d = 1;
-	for (d = 1; (d << 1) > 0; d |= d << 1)
-		printf("%d\n", d);
-	/* do { d |= d << 1; } while ((d << 1) + 1 > 0); */
-	printf("%d\n", d);
+	printf("Direct computation\n");
+	char d;
+	for (d = 1; (char) (d << 1) > 0; d |= d << 1);
+	printf("signed char\t%d:%d\n", ~d, d);
+
+	unsigned char e;
+	for (e = 1; (unsigned char) (e << 1 | 1) != e; e |= e << 1);
+	printf("unsigned char\t0:%d\n", e);
+
 	return 0;
 }
